9fd2bd81c8a068c06500b6abcc5488d6b59305e1
Sergo <80786879+SergoPetrovich@users.noreply.github.com>
2023-04-28 10:51:07 +0300

    Update bar_graf.py

--- bar_graf.py
+++ bar_graf.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_lightweight_charts as stlc
 from streamlit_lightweight_charts import renderLightweightCharts
 import streamlit_lightweight_charts.dataSamples as data
 
